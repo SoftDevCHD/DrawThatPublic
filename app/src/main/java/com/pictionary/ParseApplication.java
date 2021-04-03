861a7b34fd169f40897ea71583f0ad66a4c9fa3b
Clarence-Hugues Domond <clarencehd@outlook.com>
2021-04-03 11:03:26 -0700

    First commit

--- app/src/main/java/com/pictionary/ParseApplication.java
+++ app/src/main/java/com/pictionary/ParseApplication.java
@@ -14,12 +14,10 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Phrase.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("YY2PjRR8IDpkQ6K0ufxTvqRdUu5pfn70TSfn5JnN")
-                .clientKey("Y0GaPpCcPq6mNOhghVU3FwHCG1r65fKldTA7ghOM")
+                .applicationId("")
+                .clientKey("")
                 .server("https://parseapi.back4app.com")
                 .build()
         );
-
-
     }
 }
