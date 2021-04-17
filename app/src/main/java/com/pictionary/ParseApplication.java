5c52eec45169e00f43401950587734163aaf757c
Andy Peng <48707379+sjpmpzx@users.noreply.github.com>
2021-04-16 17:34:33 -0700

    Hide api secret using build config

--- app/src/main/java/com/pictionary/ParseApplication.java
+++ app/src/main/java/com/pictionary/ParseApplication.java
@@ -6,6 +6,9 @@ import com.parse.Parse;
 import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
+    private static final String BACK4APP_APPLICATION_ID = BuildConfig.BACK4APP_APPLICATION_ID;
+    private static final String BACK4APP_CLIENT_KEY = BuildConfig.BACK4APP_CLIENT_KEY;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -14,8 +17,8 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Phrase.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("")
-                .clientKey("")
+                .applicationId(BACK4APP_APPLICATION_ID)
+                .clientKey(BACK4APP_CLIENT_KEY)
                 .server("https://parseapi.back4app.com")
                 .build()
         );
