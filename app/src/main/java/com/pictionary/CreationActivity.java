4a15f7bcf6ce69b1a6b7ce582b0a6b15608c761d
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2021-04-30 17:20:16 -0700

    Added post functionality

--- app/src/main/java/com/pictionary/CreationActivity.java
+++ app/src/main/java/com/pictionary/CreationActivity.java
@@ -3,10 +3,22 @@ package com.pictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import org.parceler.Parcels;
+
+import java.io.File;
 
 public class CreationActivity extends AppCompatActivity {
 
@@ -16,11 +28,14 @@ public class CreationActivity extends AppCompatActivity {
     private Button btnSubmit;
     private ImageView ivCreatePicture;
     private EditText etDescription;
+    private Phrase currentPhrase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
+
+        currentPhrase = Parcels.unwrap(getIntent().getParcelableExtra("currentPhrase"));
 
         btnTakePicture = findViewById(R.id.btnTakePicture);
         btnSubmit = findViewById(R.id.btnSubmit);
@@ -38,6 +53,38 @@ public class CreationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Create post, put it in recycler view
+                String description = etDescription.getText().toString();
+                File image = null;      // TODO: Use actual image
+                ParseUser user = ParseUser.getCurrentUser();
+                Phrase phrase = currentPhrase;
+
+                savePost(description, image, user, phrase);
+            }
+        });
+    }
+
+    // Saves a new post to Parse
+    private void savePost(String description, File image, ParseUser user, Phrase phrase) {
+        ParseObject post = new ParseObject("Post");
+
+        // Set up post to be saved
+        post.put("description", description);
+        //post.put("image", image);     Uncomment this after implementing camera
+        post.put("user", user);
+        post.put("phrase", phrase);
+
+        // Save post
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                // If e exists, error has occurred
+                if(e != null) {
+                    Log.e(TAG, "Could not save post.", e);
+                    Toast.makeText(CreationActivity.this, "Post failed!", Toast.LENGTH_SHORT).show();
+                }
+                etDescription.setText("");
+                ivCreatePicture.setImageResource(0);
+                Toast.makeText(CreationActivity.this, "Posted to DrawThat!", Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/pictionary/DetailActivity.java
+++ app/src/main/java/com/pictionary/DetailActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 
 import com.pictionary.adapters.PostsAdapter;
 
+import org.parceler.Parcels;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +24,14 @@ public class DetailActivity extends AppCompatActivity {
     private RecyclerView rvPosts;
     private PostsAdapter postsAdapter;
     private List<String> posts;
+    private Phrase currentPhrase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        currentPhrase = Parcels.unwrap(getIntent().getParcelableExtra("currentPhrase"));
 
         btnCreatePost = findViewById(R.id.btnCreatePost);
         rvPosts = findViewById(R.id.rvPosts);
@@ -48,6 +53,7 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent postIntent = new Intent(DetailActivity.this, CreationActivity.class);
+                postIntent.putExtra("currentPhrase", Parcels.wrap(currentPhrase));
                 startActivity(postIntent);
             }
         });
