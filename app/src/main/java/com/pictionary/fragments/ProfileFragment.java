75afed2c60afd5a1b4bdceb402ab914fa9924e6a
Andy Peng <48707379+sjpmpzx@users.noreply.github.com>
2021-05-14 22:19:03 -0700

    UI enhencments and two more fragments completed

--- app/src/main/java/com/pictionary/fragments/ProfileFragment.java
+++ app/src/main/java/com/pictionary/fragments/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -19,11 +20,14 @@ import com.pictionary.DetailActivity;
 import com.pictionary.Post;
 import com.pictionary.R;
 
+import java.util.Date;
 import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
     public static final String TAG = "ProfileFragment";
+    private TextView tvUsername;
+    private TextView tvCreatedAt;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -39,9 +43,19 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvCreatedAt = view.findViewById(R.id.tvCreatedAt);
+
+        tvUsername.setText(ParseUser.getCurrentUser().getUsername());
+        tvCreatedAt.setText("Joined at " + ParseUser.getCurrentUser().getCreatedAt().toString());
+
     }
 
-    /** protected void queryPosts() {
+    private void queryUser() {
+        Date createdAt = ParseUser.getCurrentUser().getCreatedAt();
+    }
+
+    /* protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
         query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
--- app/src/main/java/com/pictionary/fragments/StatusFragment.java
+++ app/src/main/java/com/pictionary/fragments/StatusFragment.java
@@ -9,12 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.pictionary.Game;
+import com.pictionary.Post;
 import com.pictionary.R;
 
 public class StatusFragment extends Fragment {
 
     public static final String TAG = "StatusFragment";
+    private TextView tvNumGamesCount;
+    private TextView tvNumPostCount;
 
     public StatusFragment() {
         // Required empty public constructor
@@ -30,5 +39,31 @@ public class StatusFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        tvNumGamesCount = view.findViewById(R.id.tvNumGamesCount);
+        tvNumPostCount = view.findViewById(R.id.tvNumPostCount);
+        countQuery();
+
+    }
+
+    private void countQuery() {
+        ParseQuery<Game> query = ParseQuery.getQuery(Game.class);
+        query.whereEqualTo("createdBy", ParseUser.getCurrentUser());
+        query.countInBackground((count, e) -> {
+            if (e == null) {
+                tvNumGamesCount.setText(Integer.toString(count));
+            } else {
+                Toast.makeText(this.getContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ParseQuery<Post> query2 = ParseQuery.getQuery(Post.class);
+        query.whereEqualTo("user", ParseUser.getCurrentUser());
+        query.countInBackground((count, e) -> {
+            if (e == null) {
+                tvNumPostCount.setText(Integer.toString(count));
+            } else {
+                Toast.makeText(this.getContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
